dc7a2c068ec8d54455295df613bb2b28a77826b6
ghumman3131 <charanghumman@gmail.com>
2019-01-18 13:39:26 +0530

    first commit

---
+++ app/src/main/java/com/inception/linearlayoutweights/MainActivity.java
@@ -0,0 +1,13 @@
+package com.inception.linearlayoutweights;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
